72b15d3cfb6d6141de749d759df342ad8faaff47
Taru Dev Tewari <66220535+tarudev@users.noreply.github.com>
2023-03-13 17:21:24 +0530

    Update IncrementalClustering.cpp

--- IncrementalClustering.cpp
+++ IncrementalClustering.cpp
@@ -1,7 +1,7 @@
 #include"projectlib.h"
 
 int main(){
-   vector<point> allpoin;
+   vector<point> all_points;
    vector<point> cluster;
    int k;
 
@@ -9,31 +9,31 @@ int main(){
    cin>>k;
 
    string traindata="19 2,21 0,-3 -4,3 5,1 12,-1 -4,4 7,2.2 0.3,-6 3,2 1";
-   fetch(traindata,allpoin);
-   if(allpoin.size()<k){return 1;}
+   fetch(traindata,all_points);
+   if(all_points.size()<k){return 1;}
    cout<<"All points:\n";
-   printa(allpoin);
-   makeclus(allpoin,cluster,k);
+   print_points(all_points);
+   makeclus(all_points,cluster,k);
    cout<<"\n";
    cout<<"Initial Cluster centroids\n";
-   printw(cluster);
+   print_centroid(cluster);
    cout<<"\n";
    int k1=0;
    while(k1==0){
-       nearclusallo(allpoin,cluster);
+       near_cluster_alloted(all_points,cluster);
        k1=centroid(allpoin,cluster);
     }
-   cluspoint(allpoin,k);
-   printw(cluster);
+   clusterpoint(allpoin,k);
+   print_centroid(cluster);
    int x,y;
    cout<<"\nEnter the coordinates of the entry:\n";
    cout<<"x ="; cin>>x;
    cout<<"y ="; cin>>y;
    cout<<"\n";
-   addp(x,y,allpoin);
-   nearclusalloincre(allpoin[allpoin.size()-1],cluster);
-   ccentroid(allpoin[allpoin.size()-1].f,allpoin,cluster);
-   cluspoint(allpoin,k);
+   addp(x,y,all_points);
+   near_cluster_alloted_incremental(all_points[all_points.size()-1],cluster);
+   c_centroid(all_points[all_points.size()-1].f,all_points,cluster);
+   clusterpoint(all_points,k);
    cout<<"\n";
-   printw(cluster);
+   print_centroid(cluster);
 }
